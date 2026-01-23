a8d797d2f1df86561b381cdd1a726e99d8ea2365
Bhawesh <bhaweshjoshi689@gmail.com>
2026-01-23 18:21:25 +0530

    Added functions to update store

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,20 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
+
+const selector = useSelector((state)=>(cart))
 
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
   },
+  
   reducers: {
     addItem: (state, action) => {
-    
+           const {name , image , cost } = action.payload;
+            //first i have to check wheter this exists in store or not
+           const exists = state.items.find((item)=>item.name===name);
+           if(exists)state.items.push({name,image,cost,quantity:1});
+           else {
+             exists.quantity++;
+           }
+
+          
+
     },
     removeItem: (state, action) => {
+      const {name} = action.payload;
+      state.items = state.items.filter((item)=>item.name!==name);
     },
-    updateQuantity: (state, action) => {
+   updateQuantity: (state, action) => {
+    const { name, quantity } = action.payload;
 
-    
-    },
+    const item = state.items.find(item => item.name === name);
+    if (item) {
+        item.quantity = quantity;
+    }
+  },
   },
 });
 
